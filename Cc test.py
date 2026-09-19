5c7140f9d9e42d8c64026de8797e75ca238b07a0
JoitSQ <murder.heaven889@gmail.com>
2026-09-19 18:16:30 +0400

    Alpha 0.0.2 - Added multinumber storage (used for future operations) - Fixed UI issues with bigger windows - Made window not resizable to conquer UI issues early on

--- Cc test.py
+++ Cc test.py
@@ -5,113 +5,11 @@ from functions import *
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import ttk
-#Variables
+from GUI import UI
 
 win = Tk()
-win.title('Calculator prototype GUI Test 0.2.0')
-win.iconbitmap(default="CalculatorCc.ico")
-win.geometry('283x400')
+ui = UI(win)
 
-#Number Frame initialize
+ui.Numberupdater()
 
-allframe=tk.Frame(win)
-bframe1=tk.Frame(allframe)
-bframe2=tk.Frame(allframe)
-bframe3=tk.Frame(allframe)
-bframe4=tk.Frame(allframe)
-bframe5=tk.Frame(allframe)
-bframe6=tk.Frame(allframe)
-
-#Button initialize
-
-BA = ttk.Button(bframe1, text ="", command = test)
-Bflt = ttk.Button(bframe1, text ="", command = test)
-B0 = ttk.Button(bframe1, text ="", command = test)
-BCC = ttk.Button(bframe1, text ="", command = test)
-Beq = ttk.Button(bframe1, text ="", command = equal)
-
-BB = ttk.Button(bframe2, text ="B", command = test)
-B1 = ttk.Button(bframe2, text ="1", command = addup1)
-B2 = ttk.Button(bframe2, text ="2", command = addup2)
-B3 = ttk.Button(bframe2, text ="3", command = addup3)
-Bplus = ttk.Button(bframe2, text ="+", command = bstoreplus)
-
-BC = ttk.Button(bframe3, text ="C", command = test)
-B4 = ttk.Button(bframe3, text ="4", command = addup4)
-B5 = ttk.Button(bframe3, text ="5", command = addup5)
-B6 = ttk.Button(bframe3, text ="6", command = addup6)
-Bmin = ttk.Button(bframe3, text ="-", command = bmin)
-
-BD = ttk.Button(bframe4, text ="D", command = test)
-B7 = ttk.Button(bframe4, text ="7", command = addup7)
-B8 = ttk.Button(bframe4, text ="8", command = addup8)
-B9 = ttk.Button(bframe4, text ="9", command = addup9)
-BTim = ttk.Button(bframe4, text ="*", command = test)
-
-BE = ttk.Button(bframe5, text ="E", command = test)
-BSK1 = ttk.Button(bframe5, text ="(", command = test)
-BSK2 = ttk.Button(bframe5, text =")", command = test)
-Bdiv = ttk.Button(bframe5, text ="/", command = test)
-Bbcksp = ttk.Button(bframe5, text ="←", command = backspace)
-
-BF = ttk.Button(bframe6, text ="F", command = test)
-BAC = ttk.Button(bframe6, text ="AC", command = ac)
-
-#Buttons pack
-
-BA.pack(side=LEFT, ipadx=15, ipady=10)
-Bflt.pack(side=LEFT, ipadx=20, ipady=10)
-B0.pack(side=LEFT, ipadx=18, ipady=10)
-Beq.pack(side=RIGHT, ipadx=18, ipady=10)
-BCC.pack(side=RIGHT, ipadx=14, ipady=10)
-
-BB.pack(side=LEFT, ipadx=15, ipady=10)
-B1.pack(side=LEFT, ipadx=18, ipady=10)
-B2.pack(side=LEFT, ipadx=18, ipady=10)
-Bplus.pack(side=RIGHT, ipadx=18, ipady=10)
-B3.pack(side=RIGHT, ipadx=18, ipady=10)
-
-BC.pack(side=LEFT, ipadx=15, ipady=10)
-B4.pack(side=LEFT, ipadx=18, ipady=10)
-B5.pack(side=LEFT, ipadx=18, ipady=10)
-Bmin.pack(side=RIGHT, ipadx=20, ipady=10)
-B6.pack(side=RIGHT, ipadx=18, ipady=10)
-
-BD.pack(side=LEFT, ipadx=15, ipady=10)
-B7.pack(side=LEFT, ipadx=18, ipady=10)
-B8.pack(side=LEFT, ipadx=18, ipady=10)
-BTim.pack(side=RIGHT, ipadx=20, ipady=10)
-B9.pack(side=RIGHT, ipadx=18, ipady=10)
-
-BE.pack(side=LEFT, ipadx=15, ipady=10)
-BSK1.pack(side=LEFT, ipadx=19, ipady=10)
-BSK2.pack(side=LEFT, ipadx=19, ipady=10)
-Bbcksp.pack(side=RIGHT, ipadx=18, ipady=10)
-Bdiv.pack(side=RIGHT, ipadx=18, ipady=10)
-
-BF.pack(side=LEFT, ipadx=15, ipady=10)
-BAC.pack(side=RIGHT, ipadx=15, ipady=10)
-
-warn=Label(win, text='В связи с некоторыми неполадками, калькулятор, до поры до времени, работает в консоли PyCharm. Приносим извинения, мы уже работаем над этим!', font=("Arial", 12))
-ver=Label(win, text='v0.2.0. GUI Update', font=("Arial", 12))
-
-
-#B5.pack(anchor=CENTER, side=LEFT, padx=(10, 10)) | As example of variables
-#        ↑Выравнивание  ↑Сторона        ↑отступ(Х,У)
-
-
-#Frame pack
-
-Label.pack(warn, anchor=N)
-Label.pack(ver, side=BOTTOM, anchor=W)
-bframe6.pack(anchor=CENTER, padx=155)
-bframe5.pack(anchor=CENTER, padx=25)
-bframe4.pack(anchor=CENTER, padx=25)
-bframe3.pack(anchor=CENTER, padx=25)
-bframe2.pack(anchor=CENTER, padx=25)
-bframe1.pack(anchor=CENTER, padx=25)
-allframe.pack(side=BOTTOM, fill=X, pady=25)
-
-
-#есть идея
 win.mainloop()
--- functions.py
+++ functions.py
@@ -2,64 +2,82 @@ from math import *
 from tkinter import messagebox
 
 class F():
-    def __init__(self):
-        pass
+    def __init__(self, ui):
+        self.ui = ui
+        self.yournumber=['']
+        self.currnumber=0
 
     def test(self):
-        messagebox.showinfo("", "THE WORLD IS YOUR CANVAS")
-        messagebox.showinfo("", "SO TAKE UP YOUR BRUSH")
-        messagebox.showinfo("", "AND PAINT")
-        messagebox.showinfo("", "THE WORLD")
-        messagebox.showerror('', 'RED.')
+        messagebox.showinfo("huh", 'get your ass outta here im still testing yk')
 
     def ButtonFunction(self):
-        b0['command'] = addup0
-        b1['command'] = addup1
-        b2['command'] = addup2
-        b3['command'] = addup3
-        b4['command'] = addup4
-        b5['command'] = addup5
-        b6['command'] = addup6
-        b7['command'] = addup7
-        b8['command'] = addup8
-        b9['command'] = addup9
-        Parenthesis1['command'] = test
-        Parenthesis2['command'] = test
-        Division['command'] = dv
-        Multiply['command'] = tm
-        Minus['command'] = bmin
-        Plus['command'] = bstoreplus
-        Equal['command'] = equal
-        b0['text'] = '0'
-        Parenthesis1['text'] = '('
-        Parenthesis2['text'] = ')'
-        Division['text'] = '/'
-        Multiply['text'] = '*'
-        Minus['text'] = '-'
-        Plus['text'] = '+'
-        Equal['text'] = '='
-        b0['state'] = DISABLED
-        b1['state'] = DISABLED
-        b2['state'] = DISABLED
-        b3['state'] = DISABLED
-        b4['state'] = DISABLED
-        b5['state'] = DISABLED
-        b6['state'] = DISABLED
-        b7['state'] = DISABLED
-        b8['state'] = DISABLED
-        b9['state'] = DISABLED
-        A['state'] = DISABLED
-        B['state'] = DISABLED
-        C['state'] = DISABLED
-        D['state'] = DISABLED
-        E['state'] = DISABLED
-        F['state'] = DISABLED
-        BracketsO['state'] = DISABLED
-        BracketsC['state'] = DISABLED
-        Division['state'] = NORMAL
-        Multiply['state'] = NORMAL
-        Minus['state'] = NORMAL
-        Plus['state'] = NORMAL
-        Equal['state'] = NORMAL
-        AC['state'] = NORMAL
-        Backspace['state'] = NORMAL
+        self.ui.b0['command'] = lambda:self.addup(0)
+        self.ui.b1['command'] = lambda:self.addup(1)
+        self.ui.b2['command'] = lambda:self.addup(2)
+        self.ui.b3['command'] = lambda:self.addup(3)
+        self.ui.b4['command'] = lambda:self.addup(4)
+        self.ui.b5['command'] = lambda:self.addup(5)
+        self.ui.b6['command'] = lambda:self.addup(6)
+        self.ui.b7['command'] = lambda:self.addup(7)
+        self.ui.b8['command'] = lambda:self.addup(8)
+        self.ui.b9['command'] = lambda:self.addup(9)
+        self.ui.BracketO['command'] = self.test
+        self.ui.BracketC['command'] = self.test
+        self.ui.Division['command'] = self.dv
+        self.ui.Multiply['command'] = self.tm
+        self.ui.Minus['command'] = self.min
+        self.ui.Plus['command'] = self.plus
+        self.ui.Equal['command'] = self.equal
+        self.ui.b0['text'] = '0'
+        self.ui.BracketO['text'] = '('
+        self.ui.BracketC['text'] = ')'
+        self.ui.Division['text'] = '/'
+        self.ui.Multiply['text'] = '*'
+        self.ui.Minus['text'] = '-'
+        self.ui.Plus['text'] = '+'
+        self.ui.Equal['text'] = '='
+        self.ui.b0['state'] = 'normal'
+        self.ui.b1['state'] = 'normal'
+        self.ui.b2['state'] = 'normal'
+        self.ui.b3['state'] = 'normal'
+        self.ui.b4['state'] = 'normal'
+        self.ui.b5['state'] = 'normal'
+        self.ui.b6['state'] = 'normal'
+        self.ui.b7['state'] = 'normal'
+        self.ui.b8['state'] = 'normal'
+        self.ui.b9['state'] = 'normal'
+        self.ui.A['state'] = 'normal'
+        self.ui.B['state'] = 'normal'
+        self.ui.C['state'] = 'normal'
+        self.ui.D['state'] = 'normal'
+        self.ui.E['state'] = 'normal'
+        self.ui.F['state'] = 'normal'
+        self.ui.BracketO['state'] = 'normal'
+        self.ui.BracketC['state'] = 'normal'
+        self.ui.Division['state'] = 'normal'
+        self.ui.Multiply['state'] = 'normal'
+        self.ui.Minus['state'] = 'normal'
+        self.ui.Plus['state'] = 'normal'
+        self.ui.Equal['state'] = 'normal'
+        self.ui.AC['state'] = 'normal'
+        self.ui.Backspace['state'] = 'normal'
+
+    def addup(self, numb):
+        self.yournumber[self.currnumber]+=str(numb)
+    def changesystems(self):
+        self.ButtonFunction()
+    def dv(self):
+        self.nextnum()
+    def tm(self):
+        self.nextnum()
+    def min(self):
+        self.nextnum()
+    def equal(self):
+        self.test()
+    def plus(self):
+        self.nextnum()
+    def ac(self):
+        self.yournumber=''
+    def nextnum(self):
+        self.yournumber.append('')
+        self.currnumber+=1
